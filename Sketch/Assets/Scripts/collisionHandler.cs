2c7ee0dfe817e4d0aed0c899494352f10a222644
MikkelS <mikkelfs@gmail.com>
2021-04-19 12:16:49 +0200

    Create collisionHandler.cs

---
+++ Sketch/Assets/Scripts/collisionHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class collisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "restartLevelPlane":
+                ReloadLevel();
+                break;
+
+            case "goal":
+                LoadNextLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    
+}
