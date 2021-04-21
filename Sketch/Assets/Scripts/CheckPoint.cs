16175fe40e8ba8e4e24cae0e164711b185bdc2df
MikkelS <mikkelfs@gmail.com>
2021-04-21 16:58:49 +0200

    Create CheckPoint.cs

---
+++ Sketch/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+    // Mainly used for the respawn points in tutorial:
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.transform.position;
+            Physics.SyncTransforms();
+        }
+    }
+}
