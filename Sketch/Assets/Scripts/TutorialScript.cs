b7be5ec7596a9f684a926b36adaabe56fb83fe76
MikkelS <mikkelfs@gmail.com>
2021-04-22 10:57:41 +0200

    Create TutorialScript.cs

---
+++ Sketch/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    public GameObject uiObject;
+    private void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider player) 
+    {
+        if(player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(15);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+}
