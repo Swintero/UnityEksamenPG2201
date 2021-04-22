c323716d076cff41cfaff683acc46f102656c1ae
MikkelS <mikkelfs@gmail.com>
2021-04-23 01:36:00 +0200

    in

---
+++ Sketch/Assets/Scripts/mouseFix.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseFix : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = true;
+
+    }
+}
